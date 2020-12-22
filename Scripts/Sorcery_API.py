a4f67f3c24390fb12d20bcf75a5bf3e69b1825da
adam <vongregor@hotmail.com>
2020-12-21 19:23:59 -0600

    Modified the API source code such that the Sorcery API is now functionable. It can perform basic commands such as fetching data from the Sorcery Database using different parameters.

--- Scripts/Sorcery_API.py
+++ Scripts/Sorcery_API.py
@@ -18,12 +18,12 @@ homepage = '''<h1>Dungeon Hero Archive</h1>
 def home():
     return homepage
 
-@app.route("api/v1/resources/sorceries/all", methods = ["GET"])
+@app.route("/api/v1/resources/sorceries/all", methods = ["GET"])
 def apiAll():
-    conn = sqlite3.connect("sorceries.db")
-    conn.row_factory = dictFactory
-    cur = conn.cursor()
-    all_books = cur.execute("Select * FROM sorceries")
+    database = sqlite3.connect("Database_Objects/sorceries.db")
+    database.row_factory = dictFactory
+    cur = database.cursor()
+    all_books = cur.execute("SELECT * FROM Sorceries").fetchall()
 
     return jsonify(all_books)
 
@@ -31,6 +31,42 @@ def apiAll():
 def pageNotFound(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-@app.route("api/v1/resources/sorceries", methods = ["GET"])
+@app.route("/api/v1/resources/sorceries", methods = ["GET"])
 def apiFilter():
     queryParameters = request.args
+    
+    name = queryParameters.get("name")
+    MPCost = queryParameters.get("mp_cost", -1, type = int)
+    element = queryParameters.get("elemental_alignment")
+    forbiddenStatus = queryParameters.get("forbidden_status", -1, type = int)
+    forbiddenStatus = bool(forbiddenStatus)
+
+    query = "SELECT * FROM Sorceries WHERE"
+    sorceryInfo = {}
+
+    if name:
+        query += " name = :sorceryName AND"
+        sorceryInfo["sorceryName"] = name
+    if (MPCost != -1):
+        query += " MPCost = :sorceryManaCost AND"
+        sorceryInfo["sorceryManaCost"] = MPCost
+    if element:
+        query += " element = :sorceryElement AND"
+        sorceryInfo["sorceryElement"] = element
+    if (forbiddenStatus != -1):
+        query += " forbiddenStatus = :sorceryForbiddenStatus AND"
+        sorceryInfo["sorceryForbiddenStatus"] = forbiddenStatus
+    if not (name or (MPCost != -1) or element or (forbiddenStatus != -1)):
+        return pageNotFound(404)
+
+    query = query[:-4] + ";"
+
+    database = sqlite3.connect("Database_Objects/sorceries.db")
+    database.row_factory = dictFactory
+    cur = database.cursor()
+
+    results = cur.execute(query, sorceryInfo).fetchall()
+    
+    return jsonify(results)
+
+app.run()
